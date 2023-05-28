9744b86c306abc66ee0c512dad1dfabc4ccefd32
tomokisun <github@tomokisun.com>
2023-05-29 08:54:24 +0900

    Add avatar (#1)

---
+++ Sources/Avatar/DefaultAvatar.swift
@@ -0,0 +1,38 @@
+import SwiftUI
+import PreviewSnapshots
+
+public struct DefaultAvatar: View {
+  public init() {}
+  
+  public var body: some View {
+    Image("empty-background", bundle: .module)
+      .overlay {
+        Image(systemName: "person")
+          .resizable()
+          .foregroundColor(Color.white)
+          .scaleEffect(0.5)
+          .aspectRatio(contentMode: .fill)
+          .clipShape(Circle())
+      }
+  }
+}
+
+struct DefaultAvatarPreviews: PreviewProvider {
+  static var previews: some View {
+    snapshots
+      .previews
+      .previewLayout(.sizeThatFits)
+  }
+
+  static var snapshots: PreviewSnapshots<Void> {
+    PreviewSnapshots(
+      configurations: [
+        .init(name: "DefaultAvatar", state: ()),
+      ],
+      configure: { _ in
+        DefaultAvatar()
+      }
+    )
+  }
+}
+
